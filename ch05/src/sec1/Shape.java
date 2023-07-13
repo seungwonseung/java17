b08e7a86574249677fcec02d2ec56039a53c7f5a
seungwonseung <sjh8281@naver.com>
2023-07-13 17:54:39 +0900

    230713

---
+++ ch05/src/sec1/Shape.java
@@ -0,0 +1,54 @@
+package sec1;
+
+public class Shape {
+    //member field, member method, 생성자, 상수
+//----------------------------------------------------------------------------------
+    public String type;
+    public int length;
+    public double area;
+//접근 제한자 : public > protected > default(생략) > private
+//public : 어떤 패키지에서도 접근할 수 있음.
+//default : 같은 패키지 내에서만 접근 가능.
+//private : 현재 클래스 내부에서만 접근할 수 있음.
+
+
+    //생성자 함수는 클래스 이름이랑 같아야함
+    //반드시 멤버 필드는 private를 거는 것이 보통임.
+//------------------------------------------------------------------------------------------------------------------
+//
+    public shape(){};
+    public Shape(String type){
+        this.type = type;  //this = 현재 클래스 내부
+    }
+    public Shape(String type, int length){
+        this.type = type;
+        this.length = length;
+
+    }
+    //멤버 메소드 ----------------------------------------------------------------------------------------
+    //명령 (행위) 는 메소드로 만듬
+    //     반환타입
+    public double calcArea(){
+        if(this.type.equals("square")){
+            this.area = this.length * this.length;
+        } else if(this.type.equals("triangle")){
+            this.area = this.length * 10 / 2;
+        } else if(this.type.equals("circle")){
+            this.area = this.length * this.length * 3.14159;
+        } else {
+            this.area = 0.0f;
+        }
+        return area;
+    }//---------------------------------------------------------------------------------------------------
+    //setter, 앞은 소문자, 그 뒤는 대문자로 시작 setMethod 값
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+//-----------------------------------------------------------------------------------------------------
---
+++ ch05/src/sec2/Example1.java
@@ -0,0 +1,24 @@
+package sec2;
+
+import sec1.Shape; //서로 패키지가 다른 경우 임포트, Shape 클래스는 현재 클래스인 Example1 클래스와 다른 패키지인 sec1이 존재하므로
+                    //import가 필요함.
+
+//String 클래스와 같이 base 클래스인 경우는 사용자 정의 클래스와 다르게
+// java 언어 안에 내장 되어 있으므로 별도의 import가 필요 없음.
+public class Example1 {
+    public static void main(String[] args) {
+        String str1 = new String();
+        String str2 = new String();
+
+        //클래스명 인스턴스명 = new 생성자함수()
+        Shape s1 = new Shape();
+        s1.type = "square";
+        s1.length = 50;
+
+        double d2 = .calcArea()
+        System.out.println("현재 도형의 면적은 ? "+s1.area);
+
+        Shape s2 = new Shape("square");
+        Shape s3 = new Shape("circle", 20);
+    }
+}
